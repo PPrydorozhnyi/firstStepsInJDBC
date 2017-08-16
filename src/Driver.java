e8fcfd88f8deef0db86510daaffb11bda2508917
Peter Pridorozhny <Peter35128@gmail.com>
2017-08-16 10:33:23 +0300

    Connecting db and processing results

---
+++ src/Driver.java
@@ -0,0 +1,34 @@
+/**
+ * Created by drake on 16/08/17.
+ */
+
+import java.sql.*;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        try {
+
+            //get a connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/acme", "root", "1q2w3e4r");
+
+            //create a statement
+            Statement myStmnt = myConn.createStatement();
+
+            //execute SQL query
+            ResultSet myRs = myStmnt.executeQuery("SELECT * FROM customers");
+
+            //process the result set
+
+            while (myRs.next()) {
+                System.out.println(myRs.getString("firstName") + " " + myRs.getString("lastName"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
